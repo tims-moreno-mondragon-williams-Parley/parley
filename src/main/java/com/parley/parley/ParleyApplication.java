940db3748136f377c41b20bed505cfb3b1f41067
Johann Williams <johann.williams01@gmail.com>
2023-08-17 11:10:11 -0500

    first commit

---
+++ src/main/java/com/parley/parley/ParleyApplication.java
@@ -0,0 +1,13 @@
+package com.parley.parley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParleyApplication.class, args);
+	}
+
+}
