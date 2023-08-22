a12bc553eb28e045f6ae74a56de021b6a63ddcc9
Johann Williams <johann.williams01@gmail.com>
2023-08-22 11:35:46 -0500

    added user ability to add new post on a topic

--- src/main/java/com/parley/parley/security/SecurityConfiguration.java
+++ src/main/java/com/parley/parley/security/SecurityConfiguration.java
@@ -37,7 +37,7 @@ public class SecurityConfiguration {
         http.authorizeHttpRequests((requests) -> requests
                         /* Pages that require authentication
                          * only authenticated users can create and edit ads */
-                        .requestMatchers("/posts", "/posts/*").authenticated()
+                        .requestMatchers("/posts", "/posts/*", "/posts/*/create").authenticated()
 
 
                         /* Pages that do not require authentication
