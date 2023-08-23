8046350a512d57a6aaa3e2f60302db99fa1402db
Johann Williams <johann.williams01@gmail.com>
2023-08-23 14:46:04 -0500

    created a partials page for use on all position posts

--- src/main/java/com/parley/parley/repositories/LikeDislikePostRepository.java
+++ src/main/java/com/parley/parley/repositories/LikeDislikePostRepository.java
@@ -1,12 +1,15 @@
 package com.parley.parley.repositories;
 
 import com.parley.parley.models.LikeDislikePost;
+import com.parley.parley.models.Post;
 import com.parley.parley.models.PostLikesId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface LikeDislikePostRepository extends JpaRepository<LikeDislikePost, PostLikesId> {
-    @Override
-    Optional<LikeDislikePost> findById(PostLikesId postLikesId);
+
+    LikeDislikePost findLikeDislikePostByPostLikesId_PostAndLikeDislike(Post postLikesId_post, String likeDislike);
+    Long countLikeDislikePostByPostLikesId_PostAndLikeDislike(Post postLikesId_post, String likeDislike);
+
 }
