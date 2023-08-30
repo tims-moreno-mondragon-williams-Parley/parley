2b8ca8a9231a887591b31d4691def31bfa4733ea
Johann Williams <johann.williams01@gmail.com>
2023-08-30 10:21:41 -0500

    removed console logs

--- src/main/resources/static/js/comment_profanity.js
+++ src/main/resources/static/js/comment_profanity.js
@@ -3,13 +3,10 @@
 $(function(){
 
     let text = "";
-    console.log("Hello")
 
     $(".checkProfanityInCommentBtn").on("click", function () {
         const index = $(".checkProfanityInCommentBtn").index(this);
-        console.log("index = " + index);
         text = $(".comment-body").eq(index).val();
-        console.log("comment = " + text);
         const url = "https://" + XRapidHost + "/json?text=" + text;
         const settings = {
             async: true,
@@ -24,8 +21,6 @@ $(function(){
 
         $.ajax(settings).done(function (response) {
             $(".comment-body").eq(index).val(response.result);
-            console.log("resp = " + response.result);
-            console.log("comment body = " + $(".comment-body").eq(index).val());
             $(".submitCommentBtn").eq(index).click();
         });
     });
