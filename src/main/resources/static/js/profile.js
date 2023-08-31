8b1add5effe74b0b861cc8015225f5faac628f64
Geniece Tims <geniece.c.tims@gmail.com>
2023-08-31 00:32:19 -0500

    completed the user profile image based on wireframe and tested button functionality

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     $("#edit-profile").click(function() {
-        window.location.href = "update-profile.html";
+        window.location.href = "/profile/update";
     });
 });
