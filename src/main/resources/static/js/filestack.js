2ffe4589d8532d38a03b7e707611966aeffe678e
Johann Williams <johann.williams01@gmail.com>
2023-08-31 11:09:20 -0500

    profile + update filestack modificaiton

--- src/main/resources/static/js/filestack.js
+++ src/main/resources/static/js/filestack.js
@@ -1,23 +1,19 @@
 window.addEventListener('DOMContentLoaded', function () {
-   const key = iGotTheKey;
-   const client = filestack.init(key);
-   const options = {
-      onUploadDone: (res) => {
-       const imageUrl = res.filesUploaded[0].url;
-       let imageConatainer = document.getElementById("uploaded-image");
-          let imageinput = document.getElementById("profile_pic");
-          imageConatainer.src = imageUrl;
-          imageinput.value = imageUrl;
-       console.log(imageinput.value);
-      }
-   }
-   const picker = client.picker(options);
-   const openBtn = document.getElementById("upload-profile-pic");
-   openBtn.addEventListener("click", () => picker.open())
-
-
-
-
+    const key = iGotTheKey;
+    const client = filestack.init(key);
+    const options = {
+        onUploadDone: (res) => {
+            const imageUrl = res.filesUploaded[0].url;
+            let imageConatainer = document.getElementById("uploaded-image");
+            let imageinput = document.getElementById("profile_pic");
+            imageConatainer.src = imageUrl;
+            imageinput.value = imageUrl;
+            console.log(imageinput.value);
+        }
+    }
+    const picker = client.picker(options);
+    const openBtn = document.getElementById("upload-profile-pic");
+    openBtn.addEventListener("click", () => picker.open())
 
 
     // BANNER PICKER
@@ -33,7 +29,7 @@ window.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-const pickerBanner = client.picker(optionsBanner);
+    const pickerBanner = client.picker(optionsBanner);
     const openBtnBanner = document.getElementById("upload-banner-pic");
     openBtnBanner.addEventListener("click", () => pickerBanner.open())
 
